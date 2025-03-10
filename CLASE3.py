044f11366c129e664877c07df9420fb369aa4b7a
franrg19 <franrg19@outlook.com>
2025-03-10 19:24:59 +0100

    primer commit

---
+++ CLASE3.py
@@ -0,0 +1,14 @@
+#1 LONGITUD DE UNA CADENA
+
+nombre ="Fran Rebollo"
+print("Longitud del nombre: ", len(nombre) )
+
+#2. CONVERTIR TEXTO A MAYUSCULAS Y MINUSCULAS
+#UPPER
+print("Esto soy yo en mayusculas ", nombre.upper())
+#LOWER
+print("Esto soy yo en minusculas ", nombre.lower())
+
+#3.SLICE
+print("primeros 3 caracteres: ", nombre[:3])
+print("ultimos 4 caracteres: ", nombre[-4:])
