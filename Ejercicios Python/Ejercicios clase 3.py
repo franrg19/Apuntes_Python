580fb8c60c1a2ec833cd0a69c76a23b4b0550ccd
franrg19 <franrg19@outlook.com>
2025-03-16 20:04:21 +0100

    Commit 16 marzo

--- Ejercicios Python/Ejercicios clase 3.py
+++ Ejercicios Python/Ejercicios clase 3.py
@@ -31,3 +31,28 @@ precio_final = precio-descuento
 
 print(f"precio con descuento: €{precio_final:.2f}" )
 print(f"Redondeado hacia arriba: €{math.ceil(precio_final)}")
+
+#EJERCICIO 4
+producto = input("Nombre de producto: ")
+Precio =float(input("Precio del producto: "))
+
+etiqueta_producto = f"PRODUCTO:{producto.upper()}- PRECIO:  {Precio:.2F}€-CODIGO:{ord(producto[0])}"
+
+print(etiqueta_producto)
+
+#EJERCICIO 5
+numeros = input("Escribe números separados por comas: ")
+
+# Convertimos en lista, eliminamos duplicados y ordenamos
+lista_numeros = list(set(map(int, numeros.split(","))))
+
+print("Lista sin duplicados y ordenada:", sorted(lista_numeros))
+
+#EJERCICIO 6
+nombre = input("Escribe tu nombre: ")
+
+# Generamos una contraseña aleatoria
+contraseña = f"{nombre[0].upper()}-{random.randint(100, 999)}-*"
+
+print(f"Tu nueva contraseña es: {contraseña}")
+
